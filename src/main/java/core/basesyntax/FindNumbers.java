752741be004b79a29c94f3caac58dbac6d9ae48b
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-21 15:43:26 +0300

    Update FindNumbers.java

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -1,6 +1,13 @@
 package core.basesyntax;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 public class FindNumbers {
+    public static void main(String args[]) {
+        getAllNumbers(" 1 223-skndkn-29ndDFIP qsk    olnfd-1   9  29wsssa;s-ls-1 2-wpskmsckn");
+    }
 
     /**
      * <p>Реализуйте метод getAllNumbers(String text) который принимает строку, cодержащую буквы,
@@ -12,7 +19,13 @@ public class FindNumbers {
      * <p>Пример: если дана строка "data 48 call 9 read13 blank0a", то в массиве должны оказаться
      * числа 96, 18, 26 и 0.</p>
      */
-    public int[] getAllNumbers(String text) {
-        return null;
+    public static int[] getAllNumbers(String text) {
+        text = text.replaceAll("[^-?0-9]+", " ");
+        String[] strArray = text.trim().split(" ");
+        int[] numbers = new int[strArray.length];
+        for (int i = 0; i < strArray.length; i++) {
+            numbers[i] = Integer.parseInt(strArray[i]) * 2;
+        }
+        return numbers;
     }
 }
