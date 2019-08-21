5cda75a6b74f0a92eabff47abc0ad6eb0665680f
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-21 18:39:14 +0300

    Update FindNumbers.java

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -1,13 +1,13 @@
 package core.basesyntax;
 
 public class FindNumbers {
-    
+
     public static int[] getAllNumbers(String text) {
         text = text.replaceAll("[^-?0-9]+", " ");
-        String[] strArray = text.trim().split(" ");
-        int[] numbers = new int[strArray.length];
-        for (int i = 0; i < strArray.length; i++) {
-            numbers[i] = Integer.parseInt(strArray[i]) * 2;
+        String[] strings = text.trim().split(" ");
+        int[] numbers = new int[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            numbers[i] = Integer.parseInt(strings[i]) * 2;
         }
         return numbers;
     }
